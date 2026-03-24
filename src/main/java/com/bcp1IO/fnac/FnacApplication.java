c97409e1e54abc629477edd1e2abe168b4101194
JMgranaBUAB <jmquesadamartin@gmail.com>
2026-03-24 08:47:38 +0100

    Initial commit

---
+++ src/main/java/com/bcp1IO/fnac/FnacApplication.java
@@ -0,0 +1,14 @@
+package com.bcp1IO.fnac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnacApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(FnacApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/bcp1IO/fnac/FnacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp1IO.fnac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
