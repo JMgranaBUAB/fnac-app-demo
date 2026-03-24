fb7a16b1bfa509affdf48ba3fd7d934fd0a0aa8d
JMgranaBUAB <jmquesadamartin@gmail.com>
2026-03-24 09:01:36 +0100

    feat: connect to DDBB fnacappdemo ok

--- src/main/java/com/bcp1IO/fnac/FnacApplication.java
+++ src/main/java/com/bcp1IO/fnac/FnacApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FnacApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(FnacApplication.class, args);
-		System.out.println("Hello World");
+		SpringApplication.run(FnacApplication.class, args);
 	}
 
 }
