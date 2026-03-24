3f8ec1ad2f6c714da77191cd9b8628cf3fc70500
JMgranaBUAB <jmquesadamartin@gmail.com>
2026-03-24 09:23:22 +0100

    feat: create Entity Product

---
+++ src/main/java/com/bcp1IO/fnac/model/Product.java
@@ -0,0 +1,53 @@
+package com.bcp1IO.fnac.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
