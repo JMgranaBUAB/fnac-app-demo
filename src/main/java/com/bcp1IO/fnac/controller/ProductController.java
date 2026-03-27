4cc782accaa57b90522fa2fb7b56c671de3c89ac
JMgranaBUAB <jmquesadamartin@gmail.com>
2026-03-27 09:49:46 +0100

    feat: add U of CRUD for Product

--- src/main/java/com/bcp1IO/fnac/controller/ProductController.java
+++ src/main/java/com/bcp1IO/fnac/controller/ProductController.java
@@ -2,9 +2,12 @@ package com.bcp1IO.fnac.controller;
 
 import com.bcp1IO.fnac.model.Product;
 import com.bcp1IO.fnac.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -34,4 +37,31 @@ public class ProductController {
     public List<Product> getAllByOrder(){
         return productService.getAllByOrder();
     }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable int id){
+        Optional<Product> foundProduct = productService.findProduct(id);
+
+        if(foundProduct.isPresent()){
+            return new ResponseEntity<>(foundProduct.get(), HttpStatus.FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProductById(@PathVariable int id, @RequestBody Product updatedProduct){
+        try{
+            //actualizo los campos si existe el id en la BBDD
+            Product product = productService.updateProduct(id, updatedProduct);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        catch(Exception exception)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 }
--- src/main/java/com/bcp1IO/fnac/service/ProductService.java
+++ src/main/java/com/bcp1IO/fnac/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -33,4 +34,30 @@ public class ProductService {
         return productRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
     }
 
+    public Optional<Product> findProduct(int id){
+        return productRepository.findById(id);
+    }
+
+
+    public Product updateProduct(int id, Product updatedProduct){
+
+        //verificamos que aun existe el objeto despues de rellenar el formulario
+        Optional<Product> foundProduct = productRepository.findById(id);
+
+        if(foundProduct.isPresent()){
+            Product existingProduct = foundProduct.get();
+
+            //Actualizamos los campos
+            existingProduct.setName(updatedProduct.getName());
+            existingProduct.setDescription(updatedProduct.getDescription());
+            existingProduct.setPrice(updatedProduct.getPrice());
+
+            //guardar el producto
+            return productRepository.save(existingProduct);
+
+        }
+
+        //si no existe el producto, enviar un mensaje de error al usuario
+        throw new RuntimeException("Product not found in DDBB with id : " + id);
+    }
 }
